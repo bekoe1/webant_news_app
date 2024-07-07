55f72e5bda6e1f11a09b1b2ac8fffede56cc9572
bekoe1 <zabulimya@gmail.com>
2024-07-07 16:56:01 +0300

    Revert "fix: fix before merge"
    This reverts commit ea2059c8a31b53b3437920e416a762d038658b48.

--- lib/presentation/all_news_page/bloc/all_news_event.dart
+++ lib/presentation/all_news_page/bloc/all_news_event.dart
@@ -3,7 +3,15 @@ part of 'all_news_bloc.dart';
 @immutable
 abstract class AllNewsEvent {}
 
-class FetchingNewsEvent extends AllNewsEvent {}
+class FetchingNewsEvent extends AllNewsEvent {
+  final int offset;
+  final int limit;
+
+  FetchingNewsEvent({
+    required this.offset,
+    required this.limit,
+  });
+}
 
 class SearchingNewsEvent extends AllNewsEvent {
   final String text;

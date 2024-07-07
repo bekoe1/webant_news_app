ea2059c8a31b53b3437920e416a762d038658b48
bekoe1 <zabulimya@gmail.com>
2024-07-07 16:48:14 +0300

    fix: fix before merge

--- lib/presentation/all_news_page/bloc/all_news_bloc.dart
+++ lib/presentation/all_news_page/bloc/all_news_bloc.dart
@@ -4,15 +4,88 @@ import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:webant_news_app/data/repo_implementation/news_and_blogs_repo_impl.dart';
 import 'package:webant_news_app/domain/models/news_article_model.dart';
+import 'package:webant_news_app/domain/repo/news_and_blogs_repo.dart';
 
 part 'all_news_event.dart';
 part 'all_news_state.dart';
 
 class AllNewsBloc extends Bloc<AllNewsEvent, AllNewsState> {
-  final NewsBlogsRepoImplementation repo = NewsBlogsRepoImplementation();
+  final NewsBlogsRepo repo = NewsBlogsRepoImplementation();
 
   AllNewsBloc() : super(AllNewsInitial()) {
     on<FetchingNewsEvent>(_onAttemptToFetchNews);
+    on<RefreshingNewsEvent>(_onAttemptToRefreshNews);
+    on<SearchingNewsEvent>(_onAttemptToSearch);
+  }
+
+  int offset = 0;
+  static const int _limit = 10;
+
+  Future<void> _fetchNews({
+    required Emitter<AllNewsState> emit,
+    String? text,
+  }) async {
+    log("$offset $text");
+    final List<NewsArticleModel> news = (state is NewsFetchedSuccessfully)
+        ? (state as NewsFetchedSuccessfully).news
+        : [];
+    final response = await repo.FetchNews(
+      offset: offset,
+      limit: _limit,
+      searchText: text,
+    );
+    offset += response.results.length;
+    emit(
+      NewsFetchedSuccessfully(
+        news: [...news, ...response.results],
+        isLoading: false,
+      ),
+    );
+  }
+
+  Future<void> _onAttemptToSearch(
+    SearchingNewsEvent event,
+    Emitter<AllNewsState> emit,
+  ) async {
+    try {
+      offset = 0;
+      emit(
+        NewsFetchedSuccessfully(
+          news: [],
+          isLoading: true,
+        ),
+      );
+      await _fetchNews(
+        emit: emit,
+        text: event.text,
+      );
+    } catch (e) {
+      emit(
+        NewsFetchedWithError(
+          error: e.toString(),
+        ),
+      );
+    }
+  }
+
+  Future<void> _onAttemptToRefreshNews(
+      RefreshingNewsEvent event, Emitter<AllNewsState> emit) async {
+    try {
+      emit(
+        NewsFetchedSuccessfully(
+          news: [],
+          isLoading: true,
+        ),
+      );
+      offset = 0;
+      await _fetchNews(emit: emit);
+    } catch (e) {
+      emit(
+        NewsFetchedWithError(
+          error: e.toString(),
+        ),
+      );
+    }
   }
 
   Future<void> _onAttemptToFetchNews(
@@ -28,17 +101,7 @@ class AllNewsBloc extends Bloc<AllNewsEvent, AllNewsState> {
           isLoading: true,
         ),
       );
-      final data = await repo.FetchNews(
-        offset: event.offset,
-        limit: event.limit,
-      );
-      log("${event.offset}offset");
-      emit(
-        NewsFetchedSuccessfully(
-          news: news + data.results,
-          isLoading: false,
-        ),
-      );
+      await _fetchNews(emit: emit);
     } catch (e) {
       emit(
         NewsFetchedWithError(
--- lib/presentation/all_news_page/bloc/all_news_event.dart
+++ lib/presentation/all_news_page/bloc/all_news_event.dart
@@ -3,15 +3,7 @@ part of 'all_news_bloc.dart';
 @immutable
 abstract class AllNewsEvent {}
 
-class FetchingNewsEvent extends AllNewsEvent {
-  final int offset;
-  final int limit;
-
-  FetchingNewsEvent({
-    required this.offset,
-    required this.limit,
-  });
-}
+class FetchingNewsEvent extends AllNewsEvent {}
 
 class SearchingNewsEvent extends AllNewsEvent {
   final String text;
